f587508db5b54df1ff7c08326a644b1f4729d0b5
unknown <support-4>
2022-11-21 21:53:21 +0530

    commit 1

---
+++ Model/ITEM_V_Dao/ProductList.js
@@ -0,0 +1,10 @@
+
+
+
+const params = new URLSearchParams();
+params.append('a', 1);
+
+const response = await fetch('https://httpbin.org/post', {method: 'POST', body: params});
+const data = await response.json();
+
+console.log(data);
